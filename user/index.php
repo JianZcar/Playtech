f1ad2c4aeec0ea86b4c062738a7f0612f4e7f2b6
Carlos Heredero <herederocarlos9@gmail.com>
2025-05-28 12:00:43 +0800

    test

--- user/index.php
+++ user/index.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <title>User Dashboard</title>
+  <link rel="icon" href="../favicon.ico" type="image/x-icon">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
   <style>
